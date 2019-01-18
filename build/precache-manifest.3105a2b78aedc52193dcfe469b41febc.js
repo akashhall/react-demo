533e05a38cbe1487d1189f196c83e823265a78c8
akashhall <akash.hall@zensar.com>
2019-01-18 17:52:19 +0530

    updates

---
+++ build/precache-manifest.3105a2b78aedc52193dcfe469b41febc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "bcbd99e53e548cec1ba7",
+    "url": "/static/js/main.bcbd99e5.chunk.js"
+  },
+  {
+    "revision": "831b7f19ac7f65c365db",
+    "url": "/static/js/1.831b7f19.chunk.js"
+  },
+  {
+    "revision": "bcbd99e53e548cec1ba7",
+    "url": "/static/css/main.733efc08.chunk.css"
+  },
+  {
+    "revision": "259e4c4d9c16f67c30331b0465af41d6",
+    "url": "/index.html"
+  }
+];
