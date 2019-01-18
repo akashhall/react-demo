300d9618fe1105ae59909ba2d88ace734a1ae0c7
akashhall <akash.hall@zensar.com>
2019-01-18 19:11:37 +0530

    Create a React app and publish it to GitHub Pages

---
+++ build/precache-manifest.bdc85c992e2dce8d807509e52c5b2f2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "29238345714ba79a8f41",
+    "url": "/react-startup/static/js/runtime~main.29238345.js"
+  },
+  {
+    "revision": "bcbd99e53e548cec1ba7",
+    "url": "/react-startup/static/js/main.bcbd99e5.chunk.js"
+  },
+  {
+    "revision": "831b7f19ac7f65c365db",
+    "url": "/react-startup/static/js/1.831b7f19.chunk.js"
+  },
+  {
+    "revision": "bcbd99e53e548cec1ba7",
+    "url": "/react-startup/static/css/main.733efc08.chunk.css"
+  },
+  {
+    "revision": "790fd1eb408f1869eff95d221fc73d18",
+    "url": "/react-startup/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.3105a2b78aedc52193dcfe469b41febc.js"
+  "/react-startup/precache-manifest.bdc85c992e2dce8d807509e52c5b2f2f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-startup/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
